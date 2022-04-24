cf84b483d9fc95552c4c942d91184955e04269ce
boss.back <boss.back@kakaocorp.com>
2022-04-25 02:17:48 +0900

    feat: 자동차 이름 생성 및 5글자가 넘는 경우 에러 기능 구현

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class CarName {
+
+    private static final int MAX_LENGTH = 5;
+
+    private String name;
+
+    public CarName(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public static void validate(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("5글자 이내로 이름을 생성할 수 있습니다.");
+        }
+    }
+
+
+}
